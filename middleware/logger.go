00916c652c9ad2e179b6cc2fe2816e10bb7ebc52
MD Waliullah <waliullah.godev@gmail.com>
2025-08-14 06:56:37 +0000

    create logger middleware

---
+++ middleware/logger.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		fmt.Println(r.Method, r.URL.Path, time.Since(start))
+
+	})
+}
