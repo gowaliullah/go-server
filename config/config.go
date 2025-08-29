6b7348da7372abe1cf23cb924c127bfac2d1ea2d
MD Waliullah <waliullah.godev@gmail.com>
2025-08-29 13:12:42 +0600

    Add configuration loading and environment variables setup

--- config/config.go
+++ config/config.go
@@ -1,7 +1,33 @@
 package config
 
+import (
+	"log"
+	"os"
+)
+
 type Config struct {
 	Version     string
 	ServiceName string
 	HttpPort    int
+}
+
+func LoadConfig() {
+	version := os.Getenv("VERSION")
+	if version == "" {
+		log.Fatal("version is required")
+		os.Exit(1)
+	}
+
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		log.Fatal("service name is required")
+		os.Exit(1)
+	}
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		log.Fatal("http port is required")
+		os.Exit(1)
+	}
+
 }
