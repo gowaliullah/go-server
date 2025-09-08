481e64631553cb9590363e5cb04d331ef4cf89d2
MD Waliullah <waliullah.godev@gmail.com>
2025-09-08 23:45:48 +0600

    CreateUser routes

--- rest/routes.go
+++ rest/routes.go
@@ -15,4 +15,6 @@ func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProduct)))
 	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProduct)))
 	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProduct)))
+
+	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUser)))
 }
