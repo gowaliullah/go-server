4e32e390cbcb2306c8da847d68fa9fa2ad61b130
MD Waliullah <waliullah.godev@gmail.com>
2025-09-10 01:30:05 +0600

    payload struct

--- util/create_jwt.go
+++ util/create_jwt.go
@@ -4,3 +4,11 @@ type Header struct {
 	Alg string `json:"alg"`
 	Typ string `json:"typ"`
 }
+
+type Payload struct {
+	Sub         string `json:"sub"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	IsShopOwner bool   `json:"is_shop_owner"`
+}
