c330bd5be36cb2eccf6b173e853fff51626f94f9
MD Waliullah <waliullah.godev@gmail.com>
2025-09-16 14:39:28 +0600

    use jwtAuth for create, update and delete

--- rest/routes.go
+++ rest/routes.go
@@ -11,10 +11,10 @@ func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	// mux.Handle("GET /habib", manager.With(http.HandlerFunc(handlers.Test)))
 
 	mux.Handle("GET /products", manager.With(http.HandlerFunc(handlers.GetProducts)))
-	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProduct)))
-	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProduct)))
+	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProduct), middleware.AuthenticateJwt))
+	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProduct), middleware.AuthenticateJwt))
 	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProduct)))
-	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProduct)))
+	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProduct), middleware.AuthenticateJwt))
 
 	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUser)))
 	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(handlers.Login)))
