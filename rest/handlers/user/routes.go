07f9811018d701ce01480a5906ad509f33cb8cca
MD Waliullah <waliullah.godev@gmail.com>
2025-09-16 16:03:04 +0600

    refactor all user_handler with receiver func

--- rest/handlers/user/routes.go
+++ rest/handlers/user/routes.go
@@ -3,11 +3,10 @@ package user
 import (
 	"net/http"
 
-	"github.com/gowaliullah/ecommerce/rest/handlers"
 	middleware "github.com/gowaliullah/ecommerce/rest/middlewares"
 )
 
-func func(h *Handler) RegisterRoutes(mux *http.ServeMux, manager *middleware.Manager) {
-	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUser)))
-	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(handlers.Login)))
+func (h *Handler) RegisterRoutes(mux *http.ServeMux, manager *middleware.Manager) {
+	mux.Handle("POST /users", manager.With(http.HandlerFunc(h.CreateUser)))
+	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(h.Login)))
 }
