8d70c2802f7742891171f30e273747d456909db7
MD Waliullah <waliullah.godev@gmail.com>
2025-09-17 11:35:52 +0600

    UpdateProduct

--- rest/handlers/product/update_product.go
+++ rest/handlers/product/update_product.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/gowaliullah/ecommerce/database"
+	"github.com/gowaliullah/ecommerce/repo"
 	"github.com/gowaliullah/ecommerce/util"
 )
 
@@ -18,10 +18,10 @@ func (h *Handler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var newProduct database.Product
+	var req ReqCreatedProduct
 
 	decoder := json.NewDecoder(r.Body)
-	err = decoder.Decode(&newProduct)
+	err = decoder.Decode(&req)
 
 	if err != nil {
 		fmt.Println(err)
@@ -29,9 +29,18 @@ func (h *Handler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	newProduct.ID = id
+	_, err = h.productRepo.Update(repo.Product{
+		ID:          id,
+		Title:       req.Title,
+		Description: req.Description,
+		Price:       req.Price,
+		ImgURL:      req.ImgURL,
+	})
 
-	database.Update(newProduct)
+	if err != nil {
+		util.SendError(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
 
 	util.SendData(w, "Successfully updated product", http.StatusOK)
 }
