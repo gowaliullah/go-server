47d11ea4cd94eedda59b9c687dbf52fd7d48e3ba
MD Waliullah <waliullah.godev@gmail.com>
2025-09-17 11:46:26 +0600

    CreateUser

--- rest/handlers/user/Create_user.go
+++ rest/handlers/user/Create_user.go
@@ -5,14 +5,22 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gowaliullah/ecommerce/database"
+	"github.com/gowaliullah/ecommerce/repo"
 	"github.com/gowaliullah/ecommerce/util"
 )
 
+type ReqCreateUser struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	IsShopOwner bool   `json:"is_shop_owner"`
+}
+
 func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	var newUser database.User
+	var req ReqCreateUser
 	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&newUser)
+	err := decoder.Decode(&req)
 
 	if err != nil {
 		fmt.Println(err)
@@ -20,7 +28,16 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	createdUser := newUser.Store()
+	createdUser, err := h.userRepo.Create(repo.User{
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		Email:       req.Email,
+		Password:    req.Password,
+		IsShopOwner: req.IsShopOwner,
+	})
+	if err != nil {
+		http.Error(w, "Internal server err", http.StatusInternalServerError)
+	}
 
 	util.SendData(w, createdUser, http.StatusCreated)
 
