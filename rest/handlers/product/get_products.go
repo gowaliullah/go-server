df5d153ba0c06899b955a30632cfbe0af1963656
MD Waliullah <waliullah.godev@gmail.com>
2025-09-17 11:23:56 +0600

    GetProducts

--- rest/handlers/product/get_products.go
+++ rest/handlers/product/get_products.go
@@ -3,10 +3,14 @@ package product
 import (
 	"net/http"
 
-	"github.com/gowaliullah/ecommerce/database"
 	"github.com/gowaliullah/ecommerce/util"
 )
 
 func (h *Handler) GetProducts(w http.ResponseWriter, r *http.Request) {
-	util.SendData(w, database.List(), 200)
+	productList, err := h.productRepo.List()
+	if err != nil {
+		util.SendError(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+	util.SendData(w, productList, http.StatusOK)
 }
