bb53517f016ec72b36e588496fe3861405f7c07d
MD Waliullah <waliullah.godev@gmail.com>
2025-09-20 11:19:06 +0600

    sql for users

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gowaliullah/ecommerce/config"
+	"github.com/gowaliullah/ecommerce/infra/db"
 	"github.com/gowaliullah/ecommerce/repo"
 	"github.com/gowaliullah/ecommerce/rest"
 	"github.com/gowaliullah/ecommerce/rest/handlers/product"
@@ -11,6 +15,14 @@ import (
 
 func Serve() {
 	cnf := config.GetConfig()
+
+	dbCon, err := db.NewConnection()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(dbCon)
 
 	productRepo := repo.NewProductRepo()
 	userRepo := repo.NewUser()
