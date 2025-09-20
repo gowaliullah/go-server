5401ec444e1db5cb36ce8ef0ed2304b559e98300
MD Waliullah <waliullah.godev@gmail.com>
2025-09-20 14:22:53 +0600

    CREATE TABLE products

---
+++ db-queries/002-create-table-product.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DOUBLE PRECISION NOT NULL,
+    imageUrl TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE products;
