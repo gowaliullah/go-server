5bfbeb19ae92777f1d4be242d5f91276ec5415ff
MD Waliullah <waliullah.godev@gmail.com>
2025-09-21 06:30:03 +0600

    NewConnection

--- infra/db/connection.go
+++ infra/db/connection.go
@@ -3,16 +3,30 @@ package db
 import (
 	"fmt"
 
+	"github.com/gowaliullah/ecommerce/config"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-func GetConnectionString() string {
-	return "user=postgres password=postgres host=localhost port=5432 dbname=goserver sslmode=disable"
+func GetConnectionString(cnf config.DBConfig) string {
+
+	connString := fmt.Sprintf(
+		"user=%c password=%c host=%c port=%c dbname=%c",
+		cnf.User,
+		cnf.Password,
+		cnf.Host,
+		cnf.Port,
+		cnf.Name,
+	)
+
+	if !cnf.EnableSSLMODE {
+		connString += " sslmode=disable"
+	}
+	return connString
 }
 
-func NewConnection() (*sqlx.DB, error) {
-	dbSource := GetConnectionString()
+func NewConnection(cnf config.DBConfig) (*sqlx.DB, error) {
+	dbSource := GetConnectionString(cnf)
 	dbCon, err := sqlx.Connect("postgres", dbSource)
 	if err != nil {
 		fmt.Println(err)
