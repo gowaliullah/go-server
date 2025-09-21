b8c6c4829b45f3588feeae19aad7d72da0c17a19
MD Waliullah <waliullah.godev@gmail.com>
2025-09-21 09:55:12 +0600

    move userRepo interface into suer/port file

---
+++ user/port.go
@@ -0,0 +1,13 @@
+package user
+
+import "github.com/gowaliullah/ecommerce/domain"
+
+type UserRepo interface {
+	Create(user domain.User) (*domain.User, error)
+	Find(email, pass string) (*domain.User, error)
+
+	// Get(userId int) (*User, error)
+	// List() ([]*User, error)
+	// Delete(userId int) error
+	// Update(user User) (*User, error)
+}
