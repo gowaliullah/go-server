6b5aae3508fc211687664d7608b8be26ba43babd
MD Waliullah <waliullah.godev@gmail.com>
2025-09-21 07:57:00 +0600

    write migrate query for products

---
+++ migrations/000002-create-products.up.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DOUBLE PRECISION NOT NULL,
+    imageUrl TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
